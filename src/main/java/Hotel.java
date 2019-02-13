a0ce29f68ab817550dc03c1ae004f770be731c55
Gabriele Filigheddu <gabriele.filigheddu@hotmail.com>
2019-02-13 14:24:28 +0000

    created hotel class with collection of bedrooms and conferencerooms

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -1,12 +1,36 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Hotel {
 
+    private ArrayList<Bedroom> bedrooms;
+    private ArrayList<ConferenceRoom> conferenceRooms;
     private String name;
 
     public Hotel(String name){
         this.name = name;
+        this.bedrooms = new ArrayList<>();
+        this.conferenceRooms = new ArrayList<>();
     }
 
     public String getName(){
         return this.name;
     }
+
+    public int bedroomCount(){
+        return bedrooms.size();
+    }
+
+    public void addBedroom(Bedroom bedroom){
+        this.bedrooms.add(bedroom);
+    }
+
+    public int conferenceRoomCount(){
+        return this.conferenceRooms.size();
+    }
+
+    public void addConferenceRoom(ConferenceRoom conferenceRoom){
+        this.conferenceRooms.add(conferenceRoom);
+    }
+
 }
--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -7,22 +7,57 @@ public class HotelTest {
 
     Hotel hotel;
     Guest guest;
-    Bedroom bedroom;
-    ConferenceRoom conferenceRoom;
+    Bedroom bedroom1;
+    Bedroom bedroom2;
+    Bedroom bedroom3;
+    ConferenceRoom conferenceRoom1;
+    ConferenceRoom conferenceRoom2;
     DiningRoom diningRoom;
 
 
     @Before
     public void before(){
         hotel = new Hotel("Hotel Hotel");
-        bedroom = new Bedroom(3, 4, "double");
+        bedroom1 = new Bedroom(3, 4, "double");
+        bedroom2 = new Bedroom(2, 2, "double");
+        bedroom3 = new Bedroom(1, 1, "single");
+
         guest = new Guest("Will Smith");
-        conferenceRoom = new ConferenceRoom("Honeycombe Suite", 20);
+        conferenceRoom1 = new ConferenceRoom("Honeycombe Suite", 20);
+        conferenceRoom2 = new ConferenceRoom("Berkley Suite", 10);
+
         diningRoom = new DiningRoom(10);
     }
 
     @Test
     public void hasName(){
         assertEquals( "Hotel Hotel", hotel.getName());
+    }
+
+
+    @Test
+    public void hasNoBedrooms(){
+        assertEquals(0, hotel.bedroomCount());
+    }
+
+    @Test
+    public void canAddBedrooms(){
+        hotel.addBedroom(bedroom1);
+        hotel.addBedroom(bedroom2);
+        hotel.addBedroom(bedroom3);
+        assertEquals(3, hotel.bedroomCount());
+    }
+
+    @Test
+    public void startsWithNoConferenceRooms(){
+        assertEquals(0, hotel.conferenceRoomCount());
+    }
+
+    @Test
+    public void hasConferenceRooms(){
+        hotel.addConferenceRoom(conferenceRoom1);
+        hotel.addConferenceRoom(conferenceRoom2);
+        assertEquals(2, hotel.conferenceRoomCount());
+
     }
 }
