a76c425cb8c63fea0b4129546b53b0c257212b29
Gabriele Filigheddu <gabriele.filigheddu@hotmail.com>
2019-02-13 12:32:37 +0000

    can add guest to room

--- src/main/java/ConferenceRoom.java
+++ src/main/java/ConferenceRoom.java
@@ -1,21 +1,35 @@
+import java.util.ArrayList;
+
 public class ConferenceRoom {
 
     private String name;
     private int capacity;
+    private ArrayList<Guest> guests;
 
     public ConferenceRoom(String name, Integer capacity){
         this.name = name;
         this.capacity = capacity;
+        this.guests = new ArrayList<>();
     }
 
     public String getName(){
         return this.name;
     }
 
+    public int getCapacity(){
+        return this.capacity;
+    }
 
+    public int conferenceRoomCount(){
+        return this.guests.size();
+    }
 
+    public void addGuest(Guest guest) {
+        if (conferenceRoomCount() < this.capacity) {
+            this.guests.add(guest);
+        }
 
-
+    }
 
 
 
--- src/test/java/ConferenceRoomTest.java
+++ src/test/java/ConferenceRoomTest.java
@@ -6,10 +6,20 @@ import static org.junit.Assert.assertEquals;
 public class ConferenceRoomTest {
 
     ConferenceRoom conferenceRoom;
+    Guest guest1;
+    Guest guest2;
+    Guest guest3;
+    Guest guest4;
+    Guest guest5;
 
     @Before
     public void before(){
-        conferenceRoom = new ConferenceRoom("Honeycombe Suite", 20);
+        conferenceRoom = new ConferenceRoom("Honeycombe Suite", 4);
+        guest1 = new Guest("Will Smith");
+        guest2 = new Guest("Holly Willoby");
+        guest3 = new Guest("Dom Jolly");
+        guest4 = new Guest("Amir Khan");
+        guest5 = new Guest("Connor McGregor");
     }
 
     @Test
@@ -17,5 +27,24 @@ public class ConferenceRoomTest {
         assertEquals("Honeycombe Suite", conferenceRoom.getName());
     }
 
+    @Test
+    public void hasCapacity() {
+        assertEquals(4, conferenceRoom.getCapacity());
+    }
+    @Test
+    public void ConferenceRoomStartsEmpty(){
+        assertEquals(0, conferenceRoom.conferenceRoomCount());
+    }
 
-}
+    @Test
+    public void canAddGuestToConferenceRoom(){
+        conferenceRoom.addGuest(guest1);
+        conferenceRoom.addGuest(guest2);
+        conferenceRoom.addGuest(guest3);
+        conferenceRoom.addGuest(guest4);
+        conferenceRoom.addGuest(guest5);
+        assertEquals(4, conferenceRoom.conferenceRoomCount());
+    }
+
+
+    }
