e33110b0a487e4e9c831ed6fcdfe576e1412567f
Gabriele Filigheddu <gabriele.filigheddu@hotmail.com>
2019-02-13 13:24:12 +0000

    can add guest to diningRoom

--- src/main/java/DiningRoom.java
+++ src/main/java/DiningRoom.java
@@ -1,13 +1,28 @@
+import java.util.ArrayList;
+
 public class DiningRoom {
 
     private int capacity;
+    private ArrayList<Guest> guests;
 
     public DiningRoom(Integer capacity){
         this.capacity = 10;
+        this.guests = new ArrayList<>();
     }
 
     public int getCapacity(){
         return this.capacity;
     }
+
+    public int diningRoomCount(){
+        return this.guests.size();
+    }
+
+    public void addGuest(Guest guest){
+        if (diningRoomCount() < this.capacity) {
+            this.guests.add(guest);
+        }
+    }
+
 
 }
--- src/test/java/DiningRoomTest.java
+++ src/test/java/DiningRoomTest.java
@@ -6,16 +6,50 @@ import static org.junit.Assert.assertEquals;
 public class DiningRoomTest {
 
     DiningRoom diningRoom;
+    Guest guest1;
+    Guest guest2;
+    Guest guest3;
+    Guest guest4;
+    Guest guest5;
 
     @Before
     public void before(){
         diningRoom = new DiningRoom(10);
+        guest1 = new Guest("Will Smith");
+        guest2 = new Guest("Holly Willoby");
+        guest3 = new Guest("Dom Jolly");
+        guest4 = new Guest("Amir Khan");
+        guest5 = new Guest("Connor McGregor");
     }
 
     @Test
     public void hasCapacity(){
         assertEquals(10, diningRoom.getCapacity());
     }
+
+    @Test
+    public void startsOffEmpty(){
+        assertEquals(0, diningRoom.diningRoomCount());
+    }
+
+    @Test
+    public void canAddGuestToDiningRoom(){
+        diningRoom.addGuest(guest1);
+        diningRoom.addGuest(guest1);
+        diningRoom.addGuest(guest1);
+        diningRoom.addGuest(guest1);
+        diningRoom.addGuest(guest1);
+        diningRoom.addGuest(guest1);
+        diningRoom.addGuest(guest1);
+        diningRoom.addGuest(guest1);
+        diningRoom.addGuest(guest1);
+        diningRoom.addGuest(guest1);
+        diningRoom.addGuest(guest1);
+        diningRoom.addGuest(guest1);
+        diningRoom.addGuest(guest1);
+        assertEquals(10, diningRoom.diningRoomCount());
+    }
+    
 
 
 }
